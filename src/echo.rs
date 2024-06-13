84760d73fa5789a7ce8d32fd2970b64e70540533
Pedro Godinho <pedro.fernandez.godinho@gmail.com>
2024-06-13 06:36:11 +0100

    Add basic echo functionality

--- src/echo.rs
+++ src/echo.rs
@@ -1,59 +1,120 @@
 use crate::calculator::Stats;
 
-pub enum MainStatType {
+pub enum StatType {
+    // Main Stats
     AtkFlat,
     AtkMult,
     HpFlat,
     HpMult,
+    DefFlat,
     DefMult,
+    // Element Stats
     GlacioDmg,
     FusionDmg,
     ElectroDmg,
     AeroDmg,
     SpectroDmg,
     HavocDmg,
+    // Crit, ER
     EnergyRegen,
     CritRate,
     CritDmg,
     Healing,
-}
-
-pub enum SecondaryStatType {
-    AtkFlat,
-    HpFlat,
-}
-
-pub enum SubStatType {
-    HpFlat,
-    HpMult,
-    DefFlat,
-    DefMult,
-    AtkFlat,
-    AtkMult,
-    CritRate,
-    CritDmg,
-    EnergyRegen,
+    // Skill Stats
     SkillDmg,
     BasicDmg,
     HeavyDmg,
     LiberationDmg,
+    // Unrolled
     None,
 }
 
+impl StatType {
+    fn add_to_stats(&self, stats: &mut Stats, value: f64) {
+        match self {
+            // Main Stats
+            StatType::AtkFlat => stats.atk_flat += value,
+            StatType::AtkMult => stats.atk_mult += value,
+            StatType::HpFlat => stats.hp_flat += value,
+            StatType::HpMult => stats.hp_mult += value,
+            StatType::DefFlat => stats.def_flat += value,
+            StatType::DefMult => stats.def_mult += value,
+            // Element Stats
+            StatType::GlacioDmg => stats.element_dmg[0] += value,
+            StatType::FusionDmg => stats.element_dmg[1] += value,
+            StatType::ElectroDmg => stats.element_dmg[2] += value,
+            StatType::AeroDmg => stats.element_dmg[3] += value,
+            StatType::SpectroDmg => stats.element_dmg[4] += value,
+            StatType::HavocDmg => stats.element_dmg[5] += value,
+            // Crit, ER
+            StatType::EnergyRegen => stats.energy_regen += value,
+            StatType::CritRate => stats.crit_rate += value,
+            StatType::CritDmg => stats.crit_dmg += value,
+            StatType::Healing => stats.healing_bonus += value,
+            // Skill Stats
+            StatType::SkillDmg => stats.skill_dmg[0] += value,
+            StatType::BasicDmg => stats.skill_dmg[1] += value,
+            StatType::HeavyDmg => stats.skill_dmg[2] += value,
+            StatType::LiberationDmg => stats.skill_dmg[3] += value,
+            // Unrolled
+            StatType::None => (),
+        }
+    }
+    
+    fn remove_from_stats(&self, stats: &mut Stats, value: f64) {
+        match self {
+            // Main Stats
+            StatType::AtkFlat => stats.atk_flat -= value,
+            StatType::AtkMult => stats.atk_mult -= value,
+            StatType::HpFlat => stats.hp_flat -= value,
+            StatType::HpMult => stats.hp_mult -= value,
+            StatType::DefFlat => stats.def_flat -= value,
+            StatType::DefMult => stats.def_mult -= value,
+            // Element Stats
+            StatType::GlacioDmg => stats.element_dmg[0] -= value,
+            StatType::FusionDmg => stats.element_dmg[1] -= value,
+            StatType::ElectroDmg => stats.element_dmg[2] -= value,
+            StatType::AeroDmg => stats.element_dmg[3] -= value,
+            StatType::SpectroDmg => stats.element_dmg[4] -= value,
+            StatType::HavocDmg => stats.element_dmg[5] -= value,
+            // Crit, ER
+            StatType::EnergyRegen => stats.energy_regen -= value,
+            StatType::CritRate => stats.crit_rate -= value,
+            StatType::CritDmg => stats.crit_dmg -= value,
+            StatType::Healing => stats.healing_bonus -= value,
+            // Skill Stats
+            StatType::SkillDmg => stats.skill_dmg[0] -= value,
+            StatType::BasicDmg => stats.skill_dmg[1] -= value,
+            StatType::HeavyDmg => stats.skill_dmg[2] -= value,
+            StatType::LiberationDmg => stats.skill_dmg[3] -= value,
+            // Unrolled
+            StatType::None => (),
+        }
+    }
+}
+
 pub struct Echo {
-    pub main_stat_type: MainStatType,
+    pub main_stat_type: StatType,
     pub main_stat_value: f64,
-    pub secondary_stat_type: SecondaryStatType,
+    pub secondary_stat_type: StatType,
     pub secondary_stat_value: f64,
-    pub sub_stats: Vec<(SubStatType, f64)>,
+    pub sub_stats: Vec<(StatType, f64)>,
 }
 
 impl Echo {
     pub fn add_to_stats(&self, stats: &mut Stats) {
-        // todo
+        self.main_stat_type.add_to_stats(stats, self.main_stat_value);
+        self.secondary_stat_type.add_to_stats(stats, self.secondary_stat_value);
+        for (stat_type, value) in &self.sub_stats {
+            stat_type.add_to_stats(stats, *value);
+        }
     }
     
     pub fn remove_from_stats(&self, stats: &mut Stats) {
-        // todo
+        self.main_stat_type.remove_from_stats(stats, self.main_stat_value);
+        self.secondary_stat_type.remove_from_stats(stats, self.secondary_stat_value);
+        for (stat_type, value) in &self.sub_stats {
+            stat_type.remove_from_stats(stats, *value);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,92 @@
 use wuwa_calculator::{Element, SkillType};
 use wuwa_calculator::calculator::{BaseStats, Stats, Target};
+use wuwa_calculator::echo::{Echo, StatType};
 
 fn main() {
     let mut stats = Stats::new_from_base(BaseStats {hp: 7955.23, atk: 344.05, def: 900.28});
-
-    // Echo1
-    stats.crit_rate += 0.22;
-    stats.atk_flat += 150.0;
-    stats.hp_flat += 470.0;
-    stats.energy_regen += 0.10;
-    stats.atk_flat += 50.0;
-    stats.crit_dmg += 0.174;
-    stats.skill_dmg[SkillType::Liberation as usize] += 0.079;
-
+    
+    let echo1 = Echo {
+        main_stat_type: StatType::CritRate,
+        main_stat_value: 0.22,
+        secondary_stat_type: StatType::AtkFlat,
+        secondary_stat_value: 150.0,
+        sub_stats: vec![
+            (StatType::HpFlat, 470.0),
+            (StatType::EnergyRegen, 0.10),
+            (StatType::AtkFlat, 50.0),
+            (StatType::CritDmg, 0.174),
+            (StatType::LiberationDmg, 0.079),
+        ],
+    };
+    
     // Echo2 ER 26.8  ATK 84  CritDmg 15.0  Atk 50  Hp 320  Atk 8.6
-    stats.energy_regen += 0.268;
-    stats.atk_flat += 84.0;
-    stats.crit_dmg += 0.15;
-    stats.atk_flat += 50.0;
-    stats.hp_flat += 320.0;
-    stats.atk_mult += 0.086;
+    let echo2 = Echo {
+        main_stat_type: StatType::EnergyRegen,
+        main_stat_value: 0.268,
+        secondary_stat_type: StatType::AtkFlat,
+        secondary_stat_value: 84.0,
+        sub_stats: vec![
+            (StatType::CritDmg, 0.15),
+            (StatType::AtkFlat, 50.0),
+            (StatType::HpFlat, 320.0),
+            (StatType::AtkMult, 0.086),
+        ],
+    };
 
     // Echo3 Aero 30.0  Atk 100  CritRate 7.5  Atk 50  CritDmg 15.0  Heavy 9.4  HP 430
-    stats.element_dmg[Element::Aero as usize] += 0.30;
-    stats.atk_flat += 100.0;
-    stats.crit_rate += 0.075;
-    stats.atk_flat += 50.0;
-    stats.crit_dmg += 0.15;
-    stats.skill_dmg[SkillType::Heavy as usize] += 0.094;
-    stats.hp_flat += 430.0;
+    let echo3 = Echo {
+        main_stat_type: StatType::AeroDmg,
+        main_stat_value: 0.30,
+        secondary_stat_type: StatType::AtkFlat,
+        secondary_stat_value: 100.0,
+        sub_stats: vec![
+            (StatType::CritRate, 0.075),
+            (StatType::AtkFlat, 50.0),
+            (StatType::CritDmg, 0.15),
+            (StatType::HeavyDmg, 0.094),
+            (StatType::HpFlat, 430.0),
+        ],
+    };
 
     // Echo4 Atk 15.1%  HP 1915  Atk 50  Atk 8.6%  CritDmg 13.8  ER 7.6
-    stats.atk_mult += 0.151;
-    stats.hp_flat += 1915.0;
-    stats.atk_flat += 50.0;
-    stats.atk_mult += 0.086;
-    stats.crit_dmg += 0.138;
-    stats.energy_regen += 0.076;
+    let echo4 = Echo {
+        main_stat_type: StatType::AtkMult,
+        main_stat_value: 0.151,
+        secondary_stat_type: StatType::HpFlat,
+        secondary_stat_value: 1915.0,
+        sub_stats: vec![
+            (StatType::AtkFlat, 50.0),
+            (StatType::AtkMult, 0.086),
+            (StatType::CritDmg, 0.138),
+            (StatType::EnergyRegen, 0.076),
+        ],
+    };
 
     // Echo5 Atk 12.2%  HP 1550  Basic 10.1  CritDmg 17.4  Def 50
-    stats.atk_mult += 0.122;
-    stats.hp_flat += 1550.0;
-    stats.skill_dmg[SkillType::Basic as usize] += 0.101;
-    stats.crit_dmg += 0.174;
-    stats.def_flat += 50.0;
+    let echo5 = Echo {
+        main_stat_type: StatType::AtkMult,
+        main_stat_value: 0.122,
+        secondary_stat_type: StatType::HpFlat,
+        secondary_stat_value: 1550.0,
+        sub_stats: vec![
+            (StatType::BasicDmg, 0.101),
+            (StatType::CritDmg, 0.174),
+            (StatType::DefFlat, 50.0),
+        ],
+    };
+    echo1.add_to_stats(&mut stats);
+    echo2.add_to_stats(&mut stats);
+    echo3.add_to_stats(&mut stats);
+    echo4.add_to_stats(&mut stats);
+    echo5.add_to_stats(&mut stats);
 
+    // TODO add echo sets functionality. Hardcoding for now. 
     // Set bonus
     stats.atk_mult += 0.10;
     stats.element_dmg[Element::Aero as usize] += 0.10;
 
 
+    // TODO add weapon functionality. Hardcoding for now
     // Weapon
     stats.base_atk += 374.0;
     stats.crit_dmg += 0.359;
